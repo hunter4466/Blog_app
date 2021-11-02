4c9f72eba88ba1fd76e1b508f02a95bc27946177
Mario Chois <mario.chois.navarro@gmail.com>
2021-11-02 17:22:19 -0500

    Model: Created post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,15 @@
 class Post < ApplicationRecord
-  has_many :comments, foreign_key: :postId
-end
+    belongs_to :author, class_name: 'User'
+    has_many :comments
+    has_many :likes
+  
+    def self.update_post_counter(user)
+      counter = Post.where("author_id = #{user.id}").count
+      user.posts_counter = counter
+      user.save
+    end
+  
+    def self.most_recent_comments(post)
+      Comment.where("post_id = #{post.id}").order(created_at: :desc).limit(5)
+    end
+  end
