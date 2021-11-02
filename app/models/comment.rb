625339ba4914ae0141a66b4cd811eb2d38eda0e6
Mario Chois <mario.chois.navarro@gmail.com>
2021-11-02 17:26:48 -0500

    Linters: Fixed rubocop issues

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,10 +1,10 @@
 class Comment < ApplicationRecord
-    belongs_to :author, class_name: 'User'
-    belongs_to :post
-  
-    def self.update_comments_counter(post)
-      counter = Comment.where("post_id = #{post.id}").count
-      post.comments_counter = counter
-      post.save
-    end
+  belongs_to :author, class_name: 'User'
+  belongs_to :post
+
+  def self.update_comments_counter(post)
+    counter = Comment.where("post_id = #{post.id}").count
+    post.comments_counter = counter
+    post.save
   end
+end
