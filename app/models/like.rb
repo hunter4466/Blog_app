a868626387c5e277ce1ae0d34d6f776c6f9e7caa
Mario Chois <mario.chois.navarro@gmail.com>
2021-11-02 17:23:11 -0500

    Models: Created Like model

---
+++ app/models/like.rb
@@ -0,0 +1,10 @@
+class Like < ApplicationRecord
+    belongs_to :author, class_name: 'User'
+    belongs_to :post
+  
+    def self.update_likes_counter(post)
+      counter = Like.where("post_id = #{post.id}").count
+      post.likes_counter = counter
+      post.save
+    end
+  end
