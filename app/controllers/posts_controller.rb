04325a3cc5ff2d4cb248bfb7b808d1eb2672625c
Mario Chois <mario.chois.navarro@gmail.com>
2021-11-03 00:39:50 -0500

    Views: Added views for every controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,13 @@
 class PostsController < ApplicationController
-  def index; end
+  def index
+    @user = User.find(params[:user_id])
+    @posts = Post.joins(:author).where(author: { id: @user.id }).order(created_at: :desc)
+    @comments = Comment.order(created_at: :desc)
+  end
 
-  def show; end
+  def show
+    @user = User.find(params[:user_id])
+    @post = Post.find(params[:id])
+    @comments = Comment.order(created_at: :desc)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   root to: 'users#index'
-  get 'users/index'
-  get 'users/:user_id/posts', to: 'posts#index'
-  get 'users/:user_id/posts/:id', to: 'posts#show'
-  get 'users', to: 'users#index'
-  get 'users/:id', to: 'users#show'
+  get "/users/:user_id/posts", to: "posts#index", as: "user_posts"
+  get "/users/:user_id/posts/:id", to: "posts#show", as: "user_post"
+  get "/users", to: "users#index", as: "users"
+  get "/users/:id", to: "users#show", as: "user"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
