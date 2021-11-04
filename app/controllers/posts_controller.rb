5a8dbda3f46f7c26554c447fc0c2525dbaf951e0
Mario Chois <mario.chois.navarro@gmail.com>
2021-11-04 17:30:58 -0500

    Controllers: Updated controllers for correct functionality

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  before_action :update_interactions
+
   def index
     @user = User.find(params[:user_id])
     @userlist = User.all
@@ -11,5 +13,32 @@ class PostsController < ApplicationController
     @userlist = User.all
     @post = Post.find(params[:id])
     @comments = Comment.order(created_at: :desc)
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new
+    @post.title = params[:post][:title]
+    @post.text = params[:post][:text]
+    @post.author_id = params[:user_id]
+    if @post.save
+      redirect_to user_posts_url(@post.author_id)
+    else
+      flash.now[:error] = 'To-do item update failed'
+      render :new
+    end
+  end
+
+  private
+
+  def update_interactions
+    @posts = Post.all
+    @posts.each do |post|
+      Comment.update_comments_counter(post)
+      Like.update_likes_counter(post)
+    end
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,21 @@
 class UsersController < ApplicationController
+  before_action :update_interactions
+
   def index
     @users = User.all
-    @users.each do |user|
-      Post.update_post_counter(user)
-    end
   end
 
   def show
     @user = User.find(params[:id])
     @posts = User.most_recent_posts(@user)
-    @posts.each do |post|
-      Comment.update_comments_counter(post)
-      Like.update_likes_counter(post)
+  end
+
+  private
+
+  def update_interactions
+    @users = User.all
+    @users.each do |user|
+      Post.update_post_counter(user)
     end
   end
 end
