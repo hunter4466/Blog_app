7ab88cb04c61a724fcec10a3d4ff411bb9b954ce
Mario Chois <mario.chois.navarro@gmail.com>
2021-11-05 14:21:37 -0500

    Validations: added validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   has_many :posts, foreign_key: 'author_id', class_name: 'Post'
   has_many :comments, foreign_key: 'author_id', class_name: 'Comment'
   has_many :likes, foreign_key: 'author_id', class_name: 'Like'
+  validates :name, presence: true
+  validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   def self.most_recent_posts(user)
     Post.joins(:author).where(author: { id: user.id }).order(created_at: :desc).limit(3)
