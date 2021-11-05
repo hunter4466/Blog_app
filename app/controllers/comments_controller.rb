51ae9e64f1645fd02c2721160bd44750ad9309bc
Mario Chois <mario.chois.navarro@gmail.com>
2021-11-05 14:15:59 -0500

    Added flash notice to comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -9,8 +9,10 @@ class CommentsController < ApplicationController
     @comment.author_id = ApplicationController.current_user.id
     @comment.post_id = params[:id]
     if @comment.save
+      flash[:notice] = 'Comment is successfully added'
       redirect_to user_post_url(id: params[:id], user_id: params[:user_id])
     else
+      flash[:notice] = 'Comment could not be added'
       render :new
     end
   end
